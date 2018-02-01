70e7fa4994d5491dc88cedbf3ea5bf89cb261cea
bryanlee89 <minjae8989@gmail.com>
2018-02-01 12:39:27 -0800

    calendar added

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import BigCalendar from 'react-big-calendar'
+import moment from 'moment'
+require('react-big-calendar/lib/css/react-big-calendar.css') 
+
+BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment))
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BigCalendar
+      events={[]}
+      startAccessor='startDate'
+      endAccessor='endDate'
+    />
     );
   }
 }
