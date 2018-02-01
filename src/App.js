43dc8250ee1e298a7584505b1f654508ac53e20b
Steve Kim <dkim.steve@gmail.com>
2018-02-01 12:48:23 -0800

    calendar component created

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-import BigCalendar from 'react-big-calendar'
-import moment from 'moment'
-require('react-big-calendar/lib/css/react-big-calendar.css') 
-
-BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment))
+import MyCalendar from './Calendar';
 
 class App extends Component {
   render() {
     return (
-      <BigCalendar
-      events={[]}
-      startAccessor='startDate'
-      endAccessor='endDate'
-    />
+      <div className="App">
+      <MyCalendar />
+      </div>
     );
   }
 }
